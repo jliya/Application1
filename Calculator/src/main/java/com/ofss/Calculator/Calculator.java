19d016b5af33221ce65199f770e1376693264b72
Jay <jayliya95@gmail.com>
2019-04-16 12:37:26 +0530

    user2

--- Calculator/src/main/java/com/ofss/Calculator/Calculator.java
+++ Calculator/src/main/java/com/ofss/Calculator/Calculator.java
@@ -2,7 +2,7 @@ package com.ofss.Calculator;
 
 public class Calculator {
 	public static long add(long a, long b) {
-		System.out.println("a" +a +"\n" +"b" +b);
+		System.out.println("a" +a +"\n" +"b " +b);
 		return a + b;
 	}
 }
