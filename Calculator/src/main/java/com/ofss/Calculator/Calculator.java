5971607e800384ba4019241fd9a82917d1e9270a
Jay <jayliya95@gmail.com>
2019-04-16 12:39:30 +0530

    user1

--- Calculator/src/main/java/com/ofss/Calculator/Calculator.java
+++ Calculator/src/main/java/com/ofss/Calculator/Calculator.java
@@ -2,6 +2,7 @@ package com.ofss.Calculator;
 
 public class Calculator {
 	public static long add(long a, long b) {
+		System.out.println("User1");
 		return a + b;
 	}
 	public static long subtract(long a, long b) {
